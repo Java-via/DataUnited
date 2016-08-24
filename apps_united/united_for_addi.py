07afa6fe56d5b5887ff9fae5458e5b5c59dd3477
zhangjie <zhangjie@reyun.com>
2016-08-24 18:38:44 +0800

    fix bugs addi_united for a_id

--- apps_united/united_for_addi.py
+++ apps_united/united_for_addi.py
@@ -11,16 +11,22 @@ def addi_catchapps(date):
     sql = "SELECT a_pkgname, a_pkgname_list FROM t_apps_basic_united ORDER BY a_pkgname;"
     sql_addi = "SELECT * FROM t_apps_additional WHERE DATE(a_getdate) = %s AND a_pkgname IN %s ORDER BY a_pkgname;"
     sql_addi_pkg = "SELECT a_pkgname FROM t_apps_addi_united WHERE DATE (a_getdate) = %s;"
+
+    # first select a_pkgname_list from basic_united
     logging.debug("BASIC: start to select from basic_united")
     cur.execute(sql)
     logging.debug("BASIC: select from basic_united over, start to add to basic_pkg_list")
     basic_pkg_list = cur.fetchall()
+
+    # then select this day  addi_united already exist
     logging.debug("ADDI-UNITED: start to select from addi_united")
     cur.execute(sql_addi_pkg, date)
     logging.debug("ADDI-UNITED: select from addi_united is over, start to add to addi_pkg_set")
     addi_pkg_set = set(item[0] for item in cur.fetchall())
     logging.debug("Addi already has : %s", addi_pkg_set)
     logging.debug("BASIC: basic_pkg_list has all data")
+
+    # and then select from addi where pkgname in basic pkgname_list
     for pkg_list in basic_pkg_list:
         logging.debug("ADDI: start to modify pkgname_list")
         pkg_name = get_string_split(pkg_list[1], (" ", "\n"), is_remove_empty=True)
@@ -37,14 +43,21 @@ def addi_catchapps(date):
                 for app in apps[1:]:
                     update_addi_cur(addi_app, app)
             logging.debug("ENDING: inserting to addi_united")
-            sql_insert = "INSERT INTO t_apps_addi_united VALUES (%s, %s, %s, %s, %s, %s, %s, " \
+            sql_insert = "INSERT INTO t_apps_addi_united (a_pkgname, a_pkgname_list, a_name, a_name_list, " \
+                         "a_url, a_url_list, a_picurl, a_picurl_list, a_bytes, a_updatedate, " \
+                         "a_version, a_version_list, a_install_sum, a_install_list, a_like, " \
+                         "a_comment_sum, a_comment_list, a_score, a_softgame, a_softgame_list, " \
+                         "a_source_list, a_getdate) VALUES (%s, %s, %s, %s, %s, %s, %s, " \
                          "%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+
             sql_update = "UPDATE t_apps_addi_united SET a_pkgname_list = %s, a_name = %s, a_name_list = %s," \
                          " a_url = %s, a_url_list = %s, a_picurl = %s, a_picurl_list = %s, a_bytes = %s, " \
                          "a_updatedate = %s, a_version = %s, a_version_list = %s, a_install_sum = %s, " \
                          "a_install_list = %s, a_like = %s, a_comment_sum = %s, a_comment_list = %s, a_score = %s, " \
                          "a_softgame = %s, a_softgame_list = %s, a_source_list = %s, a_getdate = %s " \
                          "WHERE a_pkgname = %s"
+
+            # if addi_united is already exist then update else insert
             if addi_app[0] in addi_pkg_set:
                 logging.debug("Addi already exist : %s, update", addi_app[0])
                 cur.execute(sql_update, update_addi_items(addi_app))
